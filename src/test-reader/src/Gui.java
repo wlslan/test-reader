ffb053791193016a5c31bc8cb1bd992c8fc2ffdc
Ilgvars Ročāns <ilgvars.rocans@gmail.com>
2026-02-10 06:45:48 +0200

    Pievienota programmas aizvēršana

--- src/test-reader/src/Gui.java
+++ src/test-reader/src/Gui.java
@@ -16,10 +16,23 @@ public class Gui extends Frame {
         test=new JFileChooser();
         add(createLayoutButton);
         add(readTestButton);
+        addWindowListener(new MyWindowListener());
         setTitle("Test reader");
         setSize(900,300);
         setVisible(true);
         int returnVal=test.showOpenDialog(this);
+    }
+    private class MyWindowListener implements java.awt.event.WindowListener {
+        @Override
+        public void windowClosing(WindowEvent evt) {
+            System.exit(0);
+        }
+        @Override public void windowOpened(WindowEvent evt) { }
+        @Override public void windowClosed(WindowEvent evt) { }
+        @Override public void windowIconified(WindowEvent evt) { System.out.println("Window Iconified"); }
+        @Override public void windowDeiconified(WindowEvent evt) { System.out.println("Window Deiconified"); }
+        @Override public void windowActivated(WindowEvent evt) { System.out.println("Window Activated"); }
+        @Override public void windowDeactivated(WindowEvent evt) { System.out.println("Window Deactivated"); }
     }
     static void main(String[] args) {
         new Gui();
