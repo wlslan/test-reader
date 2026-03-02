f7a83e9d4c70162838fba74d7f3758c94c7a7984
ilgvars.rocans <ilgvars.rocans@gmail.com>
2026-03-02 15:50:48 +0200

    ListModdable mainīts

--- src/Gui/ListModdable.java
+++ src/Gui/ListModdable.java
@@ -6,23 +6,33 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ListModdable extends JList<Object> {
+public abstract class ListModdable<T> extends JList<T> {
     private JButton buttonModify,buttonAdd,buttonRemove;
-    private class EditorButtonListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent evt) {
-            Object currentFormat = DialogSelectFormat.Open(mainFrame);
-            if (currentFormat == null) {
-                return;
-            }
-            mainFrame.sceneEditor.OpenFormat((TestFormat) currentFormat);
-            mainFrame.ChangeScene(mainFrame.sceneEditor);
-        }
-    }
+    private DefaultListModel<T> listModel = new DefaultListModel<T>();
+
+    public abstract T Default();
+    public abstract void Modify(T obj);
+
     public ListModdable() {
         super();
         buttonModify=new JButton("Rediģēt");
         buttonAdd=new JButton("Pievienot");
         buttonRemove=new JButton("Izdzēst");
+        buttonModify.addActionListener(_ -> {
+            ListModdable outer=ListModdable.this;
+            if (outer.getSelectedIndex()!=-1) {
+                Modify((T) outer.listModel.get(outer.getSelectedIndex()));
+            }
+        });
+        buttonAdd.addActionListener(_ -> {
+            ListModdable outer=ListModdable.this;
+            outer.listModel.addElement(Default());
+        });
+        buttonRemove.addActionListener(_ -> {
+            ListModdable outer=ListModdable.this;
+            if (outer.getSelectedIndex()!=-1) {
+                outer.remove(outer.getSelectedIndex());
+            }
+        });
     }
 }
