13ba25810b285487e42c992fa3d5621a5d8daef4
ilgvars.rocans <ilgvars.rocans@gmail.com>
2026-04-27 16:05:58 +0300

    Konfigurēts projekts, lai varētu kompilēt kā .jar failu

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
+import module java.base;
 import Data.TestFormat;
 import Gui.MainFrame;
 import SaveData.SaveDataHandler;
 
 import javax.swing.*;
 
-void main() throws IOException, ClassNotFoundException {
+public class Main {
+    void main(String[] args) throws IOException, ClassNotFoundException {
 
-    TestFormat.testFormats = (TestFormat.TestFormats) SaveDataHandler.readFile(TestFormat.testFormats);
-    try {
-        // Set System L&F
-        UIManager.setLookAndFeel(
-                UIManager.getSystemLookAndFeelClassName());
-    } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
-             IllegalAccessException e) {
+        TestFormat.testFormats = (TestFormat.TestFormats) SaveDataHandler.readFile(TestFormat.testFormats);
+        try {
+            // Set System L&F
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
+                 IllegalAccessException e) {
+        }
+        new MainFrame();
     }
-    new MainFrame();
 }
